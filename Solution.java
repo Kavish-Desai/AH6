ec86fe40e575647a4ec7e9f5b57a9376c53315ec
Desai <kavish.desai@fiserv.com>
2023-11-01 15:20:52 -0400

    Solved AH6

--- Solution.java
+++ Solution.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.ArrayList;
 
 import javax.xml.transform.Source;
 
@@ -12,24 +13,33 @@ class Node<T> {
   }
 }
 
-class Solution {
+public class Solution {
   public static List<String> linkedListValues(Node<String> head) {
-    // todo
-    return;
+      // todo
+      List<String> list = new ArrayList<String>();
+      Node<String> temp = new Node<>("");
+      
+      while(head != null){
+        temp = head;
+        list.add(temp.val);
+        head = head.next;
+      }
+    return list;
   }
 
-  public static void main() {
+  public static void main(String[] args) {
     Node<String> a = new Node<>("a");
     Node<String> b = new Node<>("b");
     Node<String> c = new Node<>("c");
     Node<String> d = new Node<>("d");
+    //System.out.println(a.val);
     a.next = b;
     b.next = c;
     c.next = d;
 
     // a -> b -> c -> d
 
-    Solution.linkedListValues(a); 
+    System.out.println(Solution.linkedListValues(a)); 
     // -> [ "a", "b", "c", "d" ]
   }
 }
